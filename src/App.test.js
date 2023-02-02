01bcaecfd88709ec60db9ad73cb0a23b10013e1f
Fernando Cases <nandocasesgarcia@gmail.com>
2023-02-02 17:38:42 +0100

    Clean and ready to start assigment

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,4 @@
+/*
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
@@ -5,4 +6,6 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-});
+}); 
+
+*/
