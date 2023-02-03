aa3e0a56d47fe9b24f7039b9f6a4bc6332cc99e7
Fernando Cases <nandocasesgarcia@gmail.com>
2023-02-03 03:05:15 +0100

    docs: add comments in BE side to make easy follow flow

--- src/server/backend.js
+++ src/server/backend.js
@@ -5,30 +5,38 @@ const cors = require("cors");
 const monty = require("./monty.js");
 const app = express();
 
+// Define the port for the app to run on
 const PORT = 3100;
+// Define the build path for serving the client app
 const BUILD = "/../../build";
 
+// Use CORS middleware to handle cross origin resource sharing
 app.use(cors());
+// Serve the client app build directory using express.static
 app.use(express.static(path.join(__dirname, BUILD)));
+// Use body-parser to handle incoming JSON data
 app.use(bodyParser.json());
 
+// POST endpoint for simulating the Monty Hall problem
 app.post("/api/monty/", async (req, res) => {
+  // Retrieve the sample size, keep choice, and number of doors from the request body
   let sampleSize = req.body.sampleSize;
   let keep = req.body.keep;
   let numberOfDoors = req.body.numberOfDoors;
+  // Use the simulateMonty function from the monty.js module to get the simulation result
   let result = monty.simulateMonty(sampleSize, keep, numberOfDoors);
-  // console.log(req.body)
-  // console.log(result)
+  // Send the simulation result back in the response
   await res.send(result);
-  // console.log(`Simulation result returned`)
 });
 
+// GET endpoint for serving the client app
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, BUILD, "index.html"));
 });
 
+// Start the app on the defined port and log a message to the console
 app.listen(PORT, () => {
-  console.log(
-    `Monty Hall problem backend listening at http://localhost:${PORT}`
-  );
+  console.log(`
+Monty Hall problem backend listening at http://localhost:${PORT}
+`);
 });
--- src/server/monty.js
+++ src/server/monty.js
@@ -1,35 +1,51 @@
 const DECIMALS = 3;
 
+// Returns a random integer between the minimum and maximum inclusive
 const getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
+// Returns a random element from the given array
 const selectRandomArrayElement = (array) => {
+  // Return a random element from the array
   return array[Math.floor(Math.random() * array.length)];
 };
 
+// Generates an array of the specified size with incremental integers
 const generateNSizeArray = (size) => {
   let array = [];
+  // Loop through the size and push each integer to the array
   for (let i = 0; i < size; i++) {
     array.push(i);
   }
+  
   return array;
 };
 
+// Simulates the Monty Hall problem with the specified sample size, keep option, and number of doors
 const simulateMonty = (sampleSize, keep = false, numberOfDoors = 3) => {
+  
   let wins = 0;
+  
   let doors = generateNSizeArray(numberOfDoors);
+  // Loop through the sample size
   for (let i = 0; i < sampleSize; i++) {
+    
     let options = doors;
+    // Randomly choose the prize position
     let prizePosition = getRandomInt(0, numberOfDoors - 1);
+    // Randomly choose the player's pick
     let playerPick = getRandomInt(0, numberOfDoors - 1);
+    // Show options are the doors that don't contain the prize or the player's pick
     let showOptions = doors.filter(
       (door) => door !== prizePosition && door !== playerPick
     );
     let showed = selectRandomArrayElement(showOptions);
+    // Remove the showed door from the options
     options = options.filter((option) => option !== showed);
+    // If the player does not keep their pick, randomly choose a different option
     if (!keep) {
       playerPick = selectRandomArrayElement(
         options.filter((option) => option !== playerPick)
@@ -39,6 +55,7 @@ const simulateMonty = (sampleSize, keep = false, numberOfDoors = 3) => {
       wins += 1;
     }
   }
+  // Return an object with the simulation results
   return {
     sampleSize: Number(sampleSize),
     wins: wins,
@@ -48,7 +65,5 @@ const simulateMonty = (sampleSize, keep = false, numberOfDoors = 3) => {
   };
 };
 
-// console.log(simulateMonty(10000,false,3))
-// console.log(simulateMonty(10000,true,3))
-
+// Export the simulateMonty function
 module.exports = { simulateMonty };
