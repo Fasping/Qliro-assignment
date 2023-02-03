b69baab96bd88d37a4207fa730391ec8952cb5d4
Fernando Cases <nandocasesgarcia@gmail.com>
2023-02-03 03:22:47 +0100

    update: ReactDOM.render root index

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,21 @@
-/*
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-}); 
+afterEach(cleanup);
 
-*/
+describe("App component", () => {
+  it("should render the components correctly", () => {
+    const { getByTestId } = render(<App />);
+    expect(getByTestId("title")).toHaveTextContent(
+      "Monty Hall Problem Simulator"
+    );
+    expect(getByTestId("input")).toBeInTheDocument();
+    expect(getByTestId("strategy-toggle")).toBeInTheDocument();
+    expect(getByTestId("run-simulation")).toBeInTheDocument();
+    expect(getByTestId("frame-controls")).toBeInTheDocument();
+    expect(getByTestId("frame-results")).toBeInTheDocument();
+    expect(getByTestId("backdrop")).toBeInTheDocument();
+    expect(getByTestId("loading-backdrop")).toBeInTheDocument();
+  });
+});
